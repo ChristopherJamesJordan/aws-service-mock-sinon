6a0eb1aa83f5e4178f44ca671a9008a2ac98749b
Christopher Jordan <Christopher.James.Jordan@gmail.com>
2017-08-11 08:33:09 -0400

    updating and expanding testing suite

--- tests/awsServiceMock.test.js
+++ tests/awsServiceMock.test.js
@@ -3,16 +3,14 @@
  */
 const AWS = require('aws-sdk');
 const assert = require('assert');
-const { createAwsMock, getAwsMock, deleteAwsMock } = require('../awsServiceMock');
+const {
+  createAwsMock,
+  getAwsMock,
+  updateAwsMock,
+  deleteAwsMock,
+} = require('../awsServiceMock');
 
 describe('#awsServiceMock', function () {
-  afterEach(function() {
-    // Remove the AWS SQS mock(s) for cleaner testing
-    deleteAwsMock('SQS', 'sendMessage');
-    deleteAwsMock('S3', 'getObject');
-    deleteAwsMock('S3', 'putObject');
-  });
-
   describe('#getAwsMock @unit', function () {
     it('Should allow calledOnce assert calls on awsMock', function (done) {
       createAwsMock('S3', 'getObject', () => {
@@ -27,6 +25,7 @@ describe('#awsServiceMock', function () {
       (err, resp) => {
         assert.equal(resp.what, 'yes');
         assert.equal(getAwsMock('S3', 'getObject').calledOnce, true);
+        deleteAwsMock('S3', 'getObject');
         done();
       });
     });
@@ -45,12 +44,12 @@ describe('#awsServiceMock', function () {
       });
 
       new AWS.S3().putObject({
-        test: 'test',
-      },
-      (err, resp) => {
-        assert.equal(resp, 'hello');
-        done();
-      });
+          test: 'test',
+        }, (err, resp) => {
+          assert.equal(resp, 'hello');
+          deleteAwsMock('S3', 'putObject');
+          done();
+        });
     });
 
     it('Should allow you to use a function that uses a callback', function (done) {
@@ -63,7 +62,62 @@ describe('#awsServiceMock', function () {
       },
       (err, resp) => {
         assert.equal(resp, 'hello');
+        deleteAwsMock('S3', 'putObject');
         done();
+      });
+    });
+  });
+
+  describe('#updateAwsMock @unit', function () {
+    it('Should allow you to update and use a function that returns immediately', function (done) {
+      createAwsMock('SQS', 'sendMessage', (params, cb) => { // eslint-disable-line no-unused-vars
+        const reply = 'hello';
+        return reply;
+      });
+
+      new AWS.SQS().sendMessage({
+          test: 'test',
+        }, (err, resp) => {
+          assert.equal(resp, 'hello');
+
+          updateAwsMock('SQS', 'sendMessage', (params, cb) => { // eslint-disable-line no-unused-vars
+            const reply = 'goodbye';
+            return reply;
+          });
+
+          new AWS.SQS().sendMessage({
+              test: 'test',
+            }, (err, resp) => {
+              assert.equal(resp, 'goodbye');
+              deleteAwsMock('SQS', 'sendMessage');
+              done();
+            });
+        });
+    });
+
+    it('Should allow you to update and use a function that uses a callback', function (done) {
+      createAwsMock('SQS', 'sendMessage', (params, cb) => {
+        cb(null, 'hello');
+      });
+
+      new AWS.SQS().sendMessage({
+        test: 'test',
+      },
+      (err, resp) => {
+        assert.equal(resp, 'hello');
+
+        updateAwsMock('SQS', 'sendMessage', (params, cb) => { // eslint-disable-line no-unused-vars
+          cb(null, 'goodbye');
+        });
+
+        new AWS.SQS().sendMessage({
+            test: 'test',
+          },
+          (err, resp) => {
+            assert.equal(resp, 'goodbye');
+            deleteAwsMock('SQS', 'sendMessage');
+            done();
+          });
       });
     });
   });
@@ -76,23 +130,23 @@ describe('#awsServiceMock', function () {
       });
 
       deleteAwsMock('S3', 'putObject');
-
       assert.equal(getAwsMock('S3', 'getObject'), null);
       done();
     });
   });
 
+
   describe('#processAwsRequest @unit', function () {
     it('Should throw an error if an invalid mock is called', function (done) {
       assert.throws(
         function() {
-          new AWS.S3().putObject({
-            test: 'test',
-          },
-          (err, resp) => {
-            assert.equal(resp, 'hello');
-            done();
-          });
+          new AWS.SSM().getParameters({
+              Names: ['testParam1', 'testParam2', 'testParam3'],
+              WithDecryption: true,
+            }, (err, resp) => {
+              assert.equal(resp, 'hello');
+              done();
+            });
         },
         /No stub response for/
       );
